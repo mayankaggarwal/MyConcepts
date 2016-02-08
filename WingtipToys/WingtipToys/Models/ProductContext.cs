1c2af669795b5ce47b1f808fb6dba28f7c84806d
Mayank Aggarwal <mayankaggarwal1989@gmail.com>
2016-02-08 21:41:17 +0530

    Added Wingtip Toys project

---
+++ WingtipToys/WingtipToys/Models/ProductContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Web;
+
+namespace WingtipToys.Models
+{
+    public class ProductContext : DbContext
+    {
+        protected ProductContext() : base("WingtipToys")
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ WingtipToys/WingtipToys/Models/ProductDatabaseInitializer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace WingtipToys.Models
+{
+    public class ProductDatabaseInitializer:DropCreateDatabaseAlways<ProductContext>
+    {
+        protected override void Seed(ProductContext context)
+        {
+            GetCategories().ForEach(c => context.Categories.Add(c));
+            GetProducts().ForEach(p => context.Products.Add(p));
+        }
+
+        private static List<Category> GetCategories()
+        {
+            var categories = new List<Category>
+            {
+                new Category
+                {
+                    CategoryID = 1,
+                    CategoryName = "Cars"
+                },
+               new Category
+                {
+                    CategoryID = 2,
+                    CategoryName = "Planes"
+                },
+               new Category
+                {
+                    CategoryID = 3,
+                    CategoryName = "Trucks"
+                },
+               new Category
+                {
+                    CategoryID = 4,
+                    CategoryName = "Boats"
+                },
+               new Category
+                {
+                    CategoryID = 5,
+                    CategoryName = "Rockets"
+                }
+            };
+        }
+
+        private static List<Product> GetProducts()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
