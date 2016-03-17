00aa86d2408c86a3493ae6ea138de28c2eb00646
Mayank Aggarwal <mayankaggarwal1989@gmail.com>
2016-03-17 10:01:41 +0530

    Articles examples commited

---
+++ CSharpConcepts/CSharpConcepts/ArticlesExamples/ConceptSelectionClass.cs
@@ -0,0 +1,28 @@
+﻿using CSharpConcepts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts.ArticlesExamples
+{
+    public class ConceptSelectionClass : ConceptSelections
+    {
+        public override void SelectConcept(Enum concept)
+        {
+            base.SelectConcept(concept);
+            switch ((ArticlesExamplesConceptsList)concept)
+            {
+                case ArticlesExamplesConceptsList.DeepCloneExample:
+                    conceptExecutionClass = new DeepCloneExample();
+                    break;
+            }
+        }
+    }
+
+    public enum ArticlesExamplesConceptsList
+    {
+        DeepCloneExample
+    }
+}
---
+++ CSharpConcepts/CSharpConcepts/ArticlesExamples/DeepCloneExample.cs
@@ -0,0 +1,49 @@
+﻿using CSharpConcepts.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace CSharpConcepts.ArticlesExamples
+{
+    public class DeepCloneExample:IMainMethod
+    {
+        public void SummaryMethod()
+        {
+            Console.WriteLine("Deep Clone/Copy example");
+        }
+
+        public void MainMethod()
+        {
+            Console.WriteLine("Class implemented but but still not utilized------------------------------");
+        }
+    }
+
+    public static class ObjectCopier
+    {
+        public static T Clone<T>(T source)
+        {
+            if(!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException("This must be serializable.", "source");
+            }
+
+            if(Object.ReferenceEquals(source,null))
+            {
+                return default(T);
+            }
+
+            IFormatter formatter = new BinaryFormatter();
+            Stream stream = new MemoryStream();
+            using(stream)
+            {
+                formatter.Serialize(stream, source);
+                stream.Seek(0, SeekOrigin.Begin);
+                return (T)formatter.Deserialize(stream);
+            }
+        }
+    }
+}
