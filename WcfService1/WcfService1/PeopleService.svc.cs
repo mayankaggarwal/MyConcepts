e5d4471e0aa68163cf6304d854a7a92693570d82
Mayank Aggarwal <mayankaggarwal1989@gmail.com>
2016-04-17 20:26:07 +0530

    Removed unnecessary code

---
+++ WcfService1/WcfService1/PeopleService.svc.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfService1
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PeopleService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select PeopleService.svc or PeopleService.svc.cs at the Solution Explorer and start debugging.
+    public class PeopleService : IPeopleService
+    {
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
