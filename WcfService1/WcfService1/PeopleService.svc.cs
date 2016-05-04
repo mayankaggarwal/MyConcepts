95d79053d8b14618b0d0279ef7fea72a6a1071d1
Mayank Aggarwal <mayankaggarwal1989@gmail.com>
2016-05-04 19:11:28 +0530

    adding project for java web service

--- WcfService1/WcfService1/PeopleService.svc.cs
+++ WcfService1/WcfService1/PeopleService.svc.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
+using System.Security.Principal;
 using System.ServiceModel;
 using System.ServiceModel.Web;
 using System.Text;
+using System.Threading;
 
 namespace WcfService1
 {
@@ -12,6 +14,7 @@ namespace WcfService1
     // NOTE: In order to launch WCF Test Client for testing this service, please select PeopleService.svc or PeopleService.svc.cs at the Solution Explorer and start debugging.
     public class PeopleService : IPeopleService
     {
+        
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
