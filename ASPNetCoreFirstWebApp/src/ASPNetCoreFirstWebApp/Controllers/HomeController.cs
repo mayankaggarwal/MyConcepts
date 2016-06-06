30334b2652ca830994601cd5fce38e7178d0319b
Mayank Aggarwal <mayankaggarwal1989@gmail.com>
2016-06-06 21:35:33 +0530

    ASP DotNet core project added

---
+++ ASPNetCoreFirstWebApp/src/ASPNetCoreFirstWebApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASPNetCoreFirstWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Message = "My First ASP.NET Core RC2 running successfully!!!";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
