eae5f6b766ae21fb5a98f86a4b02aeba26560a60
Mayank Aggarwal <mayankaggarwal1989@gmail.com>
2016-07-25 20:23:37 +0530

    Node js remaining changes

--- WcfService1/ConsoleApplication1/Program.cs
+++ WcfService1/ConsoleApplication1/Program.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ConsoleApplication1.PeopleService;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +12,15 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-           
+      
+            PeopleServiceClient client = new PeopleServiceClient();
+            string value = client.GetData(45);
+            Console.WriteLine(value);
+            if(Debugger.IsAttached)
+            {
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
         }
     }
 }
--- WcfService1/WcfService1/PeopleService.svc.cs
+++ WcfService1/WcfService1/PeopleService.svc.cs
@@ -12,11 +12,16 @@ namespace WcfService1
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "PeopleService" in code, svc and config file together.
     // NOTE: In order to launch WCF Test Client for testing this service, please select PeopleService.svc or PeopleService.svc.cs at the Solution Explorer and start debugging.
-    public class PeopleService : IPeopleService
+    public class PeopleService : IPeopleService, IAnotherInterfaceService
     {
         public string GetData(int value)
         {
             return string.Format("You entered: {0}", value);
+        }
+
+        public string GetEmployeeID(int id)
+        {
+            return string.Format("Employee ID: {0}", id);
         }
     }
 }
