3ddb383b2de6d562f284d211c7af033bcd62e210
Mayank Aggarwal <mayankaggarwal1989@gmail.com>
2016-07-25 20:28:58 +0530

    WCF service

---
+++ WcfService1/WcfService1/IAnotherInterface.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WcfService1
+{
+    [ServiceContract]
+    public interface IAnotherInterfaceService
+    {
+        [OperationContract]
+        string GetEmployeeID(int id);
+    }
+}
