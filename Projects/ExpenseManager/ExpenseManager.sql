cb673add0f54ebe9a6e4cd7db22362974136b05f
Mayank Aggarwal <mayankaggarwal1989@gmail.com>
2016-08-21 19:49:06 +0530

    Expense manager

---
+++ Projects/ExpenseManager/ExpenseManager.sql
@@ -0,0 +1,53 @@
+
+CREATE TABLE ExpenseUsers
+(
+	ID INT IDENTITY(1,1) Primary Key,
+	FirstName VARCHAR(50) NOT NULL,
+	LastName VARCHAR(50) NOT NULL,
+	UserName VARCHAR(100) NOT NULL,
+	EmailID	VARCHAR(100) NOT NULL,
+	Valid BIT NOT NULL,
+	CreationDate DATETIME DEFAULT GETDATE()
+);
+go
+CREATE TABLE Groups
+(
+	ID INT IDENTITY(1,1) Primary Key,
+	GroupName VARCHAR(100) NOT NULL,
+	CreationDate DATETIME DEFAULT GETDATE(),
+	CreatedBy INT NOT NULL REFERENCES ExpenseUsers(ID)
+);
+go
+CREATE TABLE GpActorRelationship
+(
+	ID INT IDENTITY(1,1) Primary Key,
+	[Group] INT NOT NULL REFERENCES Groups(ID),
+	[User] INT NOT NULL REFERENCES ExpenseUsers(ID),
+	[Admin] BIT NOT NULL,
+	AddedDate DATETIME
+);
+go
+CREATE TABLE Activities
+(
+	ID INT IDENTITY(1,1) Primary Key,
+	[ActivityName] VARCHAR(100),
+	[Group] INT NOT NULL REFERENCES Groups(ID),
+	CreationDate	DATETIME
+);
+go
+CREATE TABLE Expensors
+(
+	ID INT IDENTITY(1,1) Primary Key,
+	[Activity]	INT NOT NULL REFERENCES Activities(ID),
+	Actor INT NOT NULL REFERENCES ExpenseUsers(ID),
+	Amount Money NOT NULL,
+);
+go
+CREATE TABLE Participants
+(
+	ID INT IDENTITY(1,1) Primary Key,
+	[Activity]	INT NOT NULL REFERENCES Activities(ID),
+	Actor INT NOT NULL REFERENCES ExpenseUsers(ID),
+	Amount Money NOT NULL
+);
+go
